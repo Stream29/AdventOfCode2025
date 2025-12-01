46b15bdf7e840a757144b980e3d5bf4acb27d200
Stream <Stream_2@qq.com>
2025-12-01 21:09:44 +0800

    feat: day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,40 @@
+fun part1(start: Int, rawInputs: List<String>): Int {
+    val inputs = rawInputs.map {
+        val direction = it[0]
+        val sign = when(direction) {
+            'L' -> -1
+            'R' -> 1
+            else -> throw IllegalArgumentException()
+        }
+        sign * it.substring(1).toInt()
+    }
+    return inputs.asSequence().runningFold(start) { acc, delta ->
+        val sum = acc + delta
+        (sum + 100) % 100
+    }.count { it == 0 }
+}
+
+fun part2(start: Int, rawInputs: List<String>): Int {
+    val inputs = rawInputs.asSequence().map {
+        val direction = it[0]
+        val sign = when (direction) {
+            'L' -> -1
+            'R' -> 1
+            else -> throw IllegalArgumentException()
+        }
+        sign * it.substring(1).toInt()
+    }
+
+    val (_, count) = inputs.fold(start to 0) { (acc, count), delta ->
+        val sum = acc + delta
+        val deltaCount = if (sum < acc) (acc - 1).floorDiv(100) - (sum - 1).floorDiv(100)
+        else sum.floorDiv(100) - acc.floorDiv(100)
+        (sum + 100) % 100 to count + deltaCount
+    }
+    return count
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(part1(50, readInput("Day01")))
+    println(part2(50, readInput("Day01")))
 }
